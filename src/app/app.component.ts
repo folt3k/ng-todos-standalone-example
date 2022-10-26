9634c1f5bc930a29a9ae3c0a661e29ea0468b990
folt3k <folt3k@gmail.com>
2022-10-26 21:40:13 +0200

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, inject } from "@angular/core";
+
+import { TodosService } from "../services/todos.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: true,
 })
-export class AppComponent {}
+export class AppComponent implements DoCheck {
+  todosService = inject(TodosService);
+
+  onButtonClick(): void {
+    console.log(this.todosService.getTodos());
+  }
+
+  ngDoCheck() {
+    console.log("change detection..");
+  }
+}
